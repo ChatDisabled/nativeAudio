7f6553ab4aba7f6d38935edcfaf67514b18ffd8e
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-08-02 19:54:43 +0200

    template files

---
+++ client.lua
@@ -0,0 +1,17 @@
+
+-- RequestScriptAudioBank will be used to load the .awc container that holds all the sounds. Follow the folder structure starting from the resource audiodirectory/custom_sounds
+
+-- The audioRef will be `special_soundset` because we called our soundset that
+-- You can use any sound that you put inside your soundset.
+
+RegisterCommand('testsound', function (source, args, raw)
+    while not RequestScriptAudioBank('audiodirectory/custom_sounds', false) do Wait(0) end
+    print('Audio bank loaded')
+    local soundId = GetSoundId()
+    PlaySoundFrontend(soundId, 'wow', 'special_soundset', true)
+    ReleaseSoundId(soundId)
+    Wait(2000)
+    local soundId2 = GetSoundId()
+    PlaySoundFrontend(soundId2, 'error', 'special_soundset', true)
+    ReleaseSoundId(soundId2)
+end, false)
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script 'client.lua'
+
+files {
+	'data/audioexample_sounds.dat54.rel',
+	'audiodirectory/custom_sounds.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'audiodirectory'
+data_file 'AUDIO_SOUNDDATA' 'data/audioexample_sounds.dat'
