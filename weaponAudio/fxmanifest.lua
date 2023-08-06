4a28b0e36df72955f907afa38be0c03488ca8225
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-08-06 15:59:00 +0200

    fix: folder path

--- weaponAudio/fxmanifest.lua
+++ weaponAudio/fxmanifest.lua
@@ -6,11 +6,11 @@ client_script 'client.lua'
 files{
 	'dlccustomweaponsounds/custom_weapon_sounds.awc',
 
-	'data/x16pistol/x16pistol_game.dat151.rel',
-	'data/x16pistol/x16pistol_sounds.dat54.rel',
+	'x16pistol/x16pistol_game.dat151.rel',
+	'x16pistol/x16pistol_sounds.dat54.rel',
 }
 
 data_file 'AUDIO_WAVEPACK' 'dlccustomweaponsounds'
 
-data_file 'AUDIO_SOUNDDATA' 'data/x16pistol/x16pistol_sounds.dat'
-data_file 'AUDIO_GAMEDATA' 'data/x16pistol/x16pistol_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'x16pistol/x16pistol_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'x16pistol/x16pistol_game.dat'
