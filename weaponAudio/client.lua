a81efb83aeeefd5dc7359e275c0a0c4d658a4b0b
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-08-06 15:03:47 +0200

    feat: weapon audio

---
+++ weaponAudio/client.lua
@@ -0,0 +1,6 @@
+CreateThread(function()
+    while true do
+        RequestScriptAudioBank('dlccustomweaponsounds/custom_weapon_sounds', false)
+        Wait(10000)
+    end
+end)
---
+++ weaponAudio/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_script 'client.lua'
+
+files{
+	'dlccustomweaponsounds/custom_weapon_sounds.awc',
+
+	'data/x16pistol/x16pistol_game.dat151.rel',
+	'data/x16pistol/x16pistol_sounds.dat54.rel',
+}
+
+data_file 'AUDIO_WAVEPACK' 'dlccustomweaponsounds'
+
+data_file 'AUDIO_SOUNDDATA' 'data/x16pistol/x16pistol_sounds.dat'
+data_file 'AUDIO_GAMEDATA' 'data/x16pistol/x16pistol_game.dat'
